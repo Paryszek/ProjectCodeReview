6e6dc993cb5e726e66a0dffc25e21f0b845e9ab0
Paryszek <michal.parysz@yahoo.com>
2019-12-06 15:48:16 +0100

    test project ready

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function attachNumber(event) {
+
+}
+
+function plusOperator() {
+
+}
+
+function minusOperator() {
+
+}
+
+function timesOperator() {
+
+}
+
+function divOperator() {
+
+}
+
+function clearOperator() {
+
+}
+
+function equalOperator() {
+
+}
+
+function updateDisplay() {
+
+}
