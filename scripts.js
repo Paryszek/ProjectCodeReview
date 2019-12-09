d04032f92a2ae4a3c3f3e88fcfee72f3e463e82e
Your Name <szymonsalamadry10@gmail.com>
2019-12-09 13:10:20 +0100

    implemented basic math operation

--- scripts.js
+++ scripts.js
@@ -1,31 +1,40 @@
-function attachNumber(event) {
+value = "";
 
+function attachNumber(event) {
+    value += event.currentTarget.value;
+    updateDisplay();
 }
 
 function plusOperator() {
-
+    value += "+";
+    updateDisplay();
 }
 
 function minusOperator() {
-
+    value += "-";
+    updateDisplay();
 }
 
 function timesOperator() {
-
+    value += "*";
+    updateDisplay();
 }
 
 function divOperator() {
-
+    value += "/";
+    updateDisplay();
 }
 
 function clearOperator() {
-
+    value = "";
+    updateDisplay();
 }
 
 function equalOperator() {
-
+    value = eval(value);
+    updateDisplay();
 }
 
 function updateDisplay() {
-
+    document.getElementById("display").value = value;
 }
